ef055c8ef1e98782c11ce6be11cf5cedfde64602
Manoucheka PLAISIMOND <plaisimondmanoucheka2@gmail.com>
2026-08-06 18:07:16 -0400

    Added profile listing and creating feature.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets
 from rest_framework import status
 
 from . import serializers
+from . import models
 
 
 # Create your views here.
@@ -104,3 +105,9 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handles removing an object."""
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewset(viewsets.ModelViewSet):
+    """Handles creating and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
