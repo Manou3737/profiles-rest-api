6ea3294d454d4ae061578ec26055874897bafbea
Manoucheka PLAISIMOND <plaisimondmanoucheka2@gmail.com>
2026-08-11 11:13:51 -0400

    Fix server setup script for Ubuntu 24.04

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -3,26 +3,20 @@
 set -e
 
 # TODO: Set to URL of git repo.
+
 PROJECT_GIT_URL='https://github.com/Manou3737/profiles-rest-api.git'
 
 PROJECT_BASE_PATH='/usr/local/apps'
 VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
-"""
-# Set Ubuntu Language
-locale-gen en_GB.UTF-8
 
-# Install Python, SQLite and pip
-echo "Installing dependencies..."
-apt-get update
-#apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
-apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
-"""
+# Install dependencies
 
 echo "Installing dependencies..."
 apt-get update
 apt-get install -y locales python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
 
 # Set Ubuntu Language
+
 locale-gen en_GB.UTF-8
 
 mkdir -p $PROJECT_BASE_PATH
@@ -34,15 +28,18 @@ python3 -m venv $VIRTUALENV_BASE_PATH/profiles_api
 $VIRTUALENV_BASE_PATH/profiles_api/bin/pip install -r $PROJECT_BASE_PATH/profiles-rest-api/requirements.txt
 
 # Run migrations
+
 cd $PROJECT_BASE_PATH/profiles-rest-api/src
 
 # Setup Supervisor to run our uwsgi process.
+
 cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
 # Setup nginx to make our application accessible.
+
 cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
