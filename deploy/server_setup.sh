f7bbfdc45be9886dd89e9e54ab247911ba979d67
Ubuntu <ubuntu@ip-172-31-34-48.ec2.internal>
2026-08-18 23:10:02 +0000

    Modernize deployment with Gunicorn and Nginx

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -2,50 +2,92 @@
 
 set -e
 
-# TODO: Set to URL of git repo.
-
 PROJECT_GIT_URL='https://github.com/Manou3737/profiles-rest-api.git'
 
 PROJECT_BASE_PATH='/usr/local/apps'
 VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
 
-# Install dependencies
+PROJECT_PATH="$PROJECT_BASE_PATH/profiles-rest-api"
+VENV_PATH="$VIRTUALENV_BASE_PATH/profiles_api"
+DJANGO_PATH="$PROJECT_PATH/src/profiles_project"
 
 echo "Installing dependencies..."
-apt-get update
-apt-get install -y locales python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
 
-# Set Ubuntu Language
+apt-get update
+apt-get install -y \
+    locales \
+    python3-dev \
+    python3-venv \
+    sqlite3 \
+    python3-pip \
+    supervisor \
+    nginx \
+    git
+
+echo "Configuring locale..."
 
 locale-gen en_GB.UTF-8
 
-mkdir -p $PROJECT_BASE_PATH
-if [ ! -d "$PROJECT_BASE_PATH/profiles-rest-api/.git" ]; then
- git clone --branch modernize-python314 $PROJECT_GIT_URL $PROJECT_BASE_PATH/profiles-rest-api
+echo "Preparing application directory..."
+
+mkdir -p "$PROJECT_BASE_PATH"
+
+if [ ! -d "$PROJECT_PATH/.git" ]; then
+    git clone --branch modernize-python314 "$PROJECT_GIT_URL" "$PROJECT_PATH"
 fi
 
-mkdir -p $VIRTUALENV_BASE_PATH
-python3 -m venv $VIRTUALENV_BASE_PATH/profiles_api
+echo "Preparing virtual environment..."
 
-$VIRTUALENV_BASE_PATH/profiles_api/bin/pip install -r $PROJECT_BASE_PATH/profiles-rest-api/requirements.txt
+mkdir -p "$VIRTUALENV_BASE_PATH"
 
-# Run migrations
+if [ ! -d "$VENV_PATH" ]; then
+    python3 -m venv "$VENV_PATH"
+fi
 
-cd $PROJECT_BASE_PATH/profiles-rest-api/src/profiles_project
-$VIRTUALENV_BASE_PATH/profiles_api/bin/python manage.py migrate
+echo "Installing Python dependencies..."
 
-# Setup Supervisor to run our uwsgi process.
+"$VENV_PATH/bin/pip" install -r "$PROJECT_PATH/requirements.txt"
 
-cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+echo "Running Django checks..."
+
+cd "$DJANGO_PATH"
+
+"$VENV_PATH/bin/python" manage.py check
+
+echo "Running migrations..."
+
+"$VENV_PATH/bin/python" manage.py migrate
+
+echo "Collecting static files..."
+
+"$VENV_PATH/bin/python" manage.py collectstatic --noinput
+
+echo "Configuring Supervisor..."
+
+SUPERVISOR_CONF="/etc/supervisor/conf.d/profiles_api.conf"
+
+if [ -f "$SUPERVISOR_CONF" ]; then
+    echo "Supervisor configuration already exists; preserving server environment variables."
+else
+    cp "$PROJECT_PATH/deploy/supervisor_profiles_api.conf" "$SUPERVISOR_CONF"
+fi
+
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
-# Setup nginx to make our application accessible.
+echo "Configuring Nginx..."
 
-cp $PROJECT_BASE_PATH/profiles-rest-api/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
-rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
-systemctl restart nginx.service
+cp "$PROJECT_PATH/deploy/nginx_profiles_api.conf" \
+   /etc/nginx/sites-available/profiles_api.conf
 
-echo "DONE! :)"
+rm -f /etc/nginx/sites-enabled/default
+
+ln -sf \
+   /etc/nginx/sites-available/profiles_api.conf \
+   /etc/nginx/sites-enabled/profiles_api.conf
+
+nginx -t
+systemctl restart nginx
+
+echo "Deployment completed successfully!"
