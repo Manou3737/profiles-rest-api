fef4e27c63a24e1abf8c9512b2862b875436af7d
Manoucheka PLAISIMOND <plaisimondmanoucheka2@gmail.com>
2026-08-21 13:52:14 -0400

    Enable JWT token blacklist

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'rest_framework.authtoken',
     'profiles_api',
 ]
